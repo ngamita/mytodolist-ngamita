1bef3f090c8cee0cf1086db25fe6ad50958de41f
Richard Ngamita <ngamita@mzee.(none)>
2010-07-08 13:47:31 +0300

    Added the User Authentication and Template changes

--- main.py
+++ main.py
@@ -23,25 +23,39 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
 from google.appengine.ext import db
+from google.appengine.api import users
 
 
 class MyTodoModel(db.Model):
+    owner = db.UserProperty()
     description = db.StringProperty()
     created = db.DateTimeProperty(auto_now_add = True) 
 
 
 
-
+#Main Page where the User can Login and Logout 
 class MainHandler(webapp.RequestHandler):
     def get(self):
+        user = users.get_current_user()
+        url = users.create_login_url(self.request.uri)
+        url_linktext = 'Login'
         
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+            
+            
+        #GQL - is kind of similar the SQL 
         #my_todo = MyTodoModel.all().order('-created').fetch(4)
-        my_todo = db.GqlQuery("SELECT * FROM MyTodoModel ORDER BY created DESC LIMIT 4")
+        my_todo = db.GqlQuery("SELECT * FROM MyTodoModel WHERE owner = :owner ORDER BY created DESC LIMIT 4" ,owner = users.get_current_user())
         template_values = {
-            'my_todo': my_todo
+            'my_todo': my_todo,
+            'num_my_todo': my_todo.count(),
+            'user':user,
+            'url': url,
+            'url_linktext': url_linktext,
         
         }
-        
     
     
         template_files = os.path.join(os.path.dirname(__file__),'index.html')
@@ -49,10 +63,14 @@ class MainHandler(webapp.RequestHandler):
         
         
     def post(self):
-        todo = MyTodoModel()
-        todo.description = self.request.get("my_todo")       
-        todo.put()
-        self.redirect('/')
+        user = users.get_current_user()
+        
+        if user:
+            todo = MyTodoModel()
+            todo.owner = users.get_current_user()
+            todo.description = self.request.get("my_todo")       
+            todo.put()
+            self.redirect('/')
         
         
         
